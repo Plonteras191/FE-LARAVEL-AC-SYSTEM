c50ff27c3c2c98f353aa3bd5ccb9c5d0001959d5
Plonteras191 <plonteras.johnkristoffer@gmail.com>
2025-06-03 08:09:23 +0800

    deploy

--- ac-system/vite.config.js
+++ ac-system/vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
+  plugins: [react()],
   base: process.env.VITE_BASE_PATH || '/FE-LARAVEL-AC-SYSTEM',
 })
